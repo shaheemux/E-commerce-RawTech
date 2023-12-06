cd7485bf58c7a58e6ff4fc0422d3e37d2e773d6c
unknown <Shaheemsalie349@gmail.com>
2023-12-05 22:49:24 -0500

    added files

--- javascript/product.js
+++ javascript/product.js
@@ -9,17 +9,17 @@ function Computer(id,name,specs,price,url){
     this.url = url;
 }
 
-let computer1 = new Computer(1, "Cosair", "Intel Core i5 + GeForce RTX 4060 Gaming PC White", 455, "https://i.postimg.cc/tgX5w8nK/asethic-pictures-of.jpg");
+let computer1 = new Computer(1, "Cosair", "Intel Core i5 + GeForce RTX 4060 Gaming PC White", "R5,999", "https://i.postimg.cc/tgX5w8nK/asethic-pictures-of.jpg");
 
-let computer2 = new Computer(2, "AMD","Ryzen 5 5600 Windows 11","2500","https://i.postimg.cc/tgX5w8nK/asethic-pictures-of.jpg");
+let computer2 = new Computer(2, "AMD","Ryzen 5 5600 Windows 11","R2,500","https://i.postimg.cc/tgX5w8nK/asethic-pictures-of.jpg");
 
-let computer3 = new Computer(3, "CoolerMaster","Ryzen 5 5500 SCOUT Windows 11 Gaming PC","6500","https://i.postimg.cc/tgX5w8nK/asethic-pictures-of.jpg");
+let computer3 = new Computer(3, "CoolerMaster","Ryzen 5 5500 SCOUT Windows 11 Gaming PC","R6,899","https://i.postimg.cc/tgX5w8nK/asethic-pictures-of.jpg");
 
-let computer4 = new Computer(4, "Gigabyte","AORUS C500","4500","https://i.postimg.cc/tgX5w8nK/asethic-pictures-of.jpg");
+let computer4 = new Computer(4, "Gigabyte","AORUS C500","R4,999","https://i.postimg.cc/tgX5w8nK/asethic-pictures-of.jpg");
 
-let computer5 = new Computer(5, "Intel Core i5","16GB 1TB Nvidia RTX 40 Series Gaming","8500","https://i.postimg.cc/tgX5w8nK/asethic-pictures-of.jpg");
+let computer5 = new Computer(5, "Intel Core i5","16GB 1TB Nvidia RTX 40 Series Gaming","R4,599","https://i.postimg.cc/tgX5w8nK/asethic-pictures-of.jpg");
 
-let computer6 = new Computer(6, " CORSAIR","Performance Plus Core I5 4060Ti Gaming PC","50000", "https://i.postimg.cc/tgX5w8nK/asethic-pictures-of.jpg");
+let computer6 = new Computer(6, " CORSAIR","Performance Plus Core I5 4060Ti Gaming PC","R6,499", "https://i.postimg.cc/tgX5w8nK/asethic-pictures-of.jpg");
 
 computerArray.push(computer1,computer2,computer3,computer4,computer5,computer6);
 
@@ -34,33 +34,9 @@ let mainContent = document.querySelector('main')
 window.onload = function () {
     let products = computerArray.map(function (item, index) {
         return `
+        
          
-        <div class="row row-cols-1 row-cols-md-3 g-4">
-  <div class="col">
-    <div class="card h-100">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
-      <div class="card-footer">
-        <small class="text-body-secondary">Last updated 3 mins ago</small>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card h-100">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      </div>
-      <div class="card-footer">
-        <small class="text-body-secondary">Last updated 3 mins ago</small>
-      </div>
-    </div>
-  </div>
-</div>
+ 
 
         ${item.name}
         ${item.specs}
